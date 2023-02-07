4280b735f256063e515fc76bca79f0839613e1b2
IqraChishty <iqrachishty@gmail.com>
2023-02-07 20:53:56 +0500

    Change base api url

--- src/services/base-api.jsx
+++ src/services/base-api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://billingappserver-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json'
     // 'Access-Control-Allow-Origin': 'http://127.0.0.1:3000'
